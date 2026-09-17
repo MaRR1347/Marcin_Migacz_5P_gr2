bf98f325c8d50a17be739bc04314a578f36a4c80
Tobiasz <ttoczek12@vp.pl>
2026-09-17 10:49:58 +0200

    Calculator app added

---
+++ Aplikacja_do_testowania/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplikacja_do_testowania
+{
+    internal class Calculator
+    {
+        static int num1, num2;
+        static void Main(string[] args) {
+            Console.WriteLine("Wpisz pierwszą liczbę");
+            int.TryParse(Console.ReadLine(), out num1);
+            Console.WriteLine("Wpisz drugą liczbę");
+            int.TryParse(Console.ReadLine(), out num2);
+
+            Console.WriteLine($"Suma wynosi: {num1 + num2}");
+
+        }
+    }
+}
---
+++ Aplikacja_do_testowania/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplikacja_do_testowania
+{
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
